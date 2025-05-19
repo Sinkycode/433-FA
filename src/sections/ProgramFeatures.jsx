b5646541bd3f69624f642191c9ddcc5022bc4483
Sinkycode <mosfreshlafrosh@gmail.com>
2025-05-19 09:44:04 +0300

    perf: optimize ProgramFeatures layout, teamManagement layout and responsiveness

--- src/sections/ProgramFeatures.jsx
+++ src/sections/ProgramFeatures.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion"; // Import motion from framer-motion
 import { Features } from "../constants";
 import { Bus } from "../assets/images/gallery";
 
@@ -9,58 +10,92 @@ const ProgramFeatures = () => {
   const rightFeatures = Features.slice(mid);
 
   return (
-    <section className="relative p-6 sm:h-screen min-h-screen flex flex-col justify-center items-center overflow-x-hidden w-full lg:py-11">
-      {/* Image start */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10 md:flex justify-center items-center w-[700px] hidden ">
-        <img src={Bus} alt="Centerpiece" className="object-contain" />
+    <section className="min-h-screen padding relative">
+      {/* Center Image - responsive width and hidden on small screens */}
+      <div className="lg:absolute top-1/2 left-1/2 transform lg:-translate-x-1/2 lg:-translate-y-1/2 lg:w-[700px]">
+        <img
+          src={Bus}
+          alt="Centerpiece"
+          className="object-contain max-w-full"
+        />
       </div>
-      {/* end */}
 
-      <div className="h-full w-full relative grid gap-5 lg:w-[70vw] mx-auto">
+      <div className="md:grid grid-cols-3 gap-6 flex flex-col items-center justify-center relative lg:h-screen">
         {/* Left column */}
-        <div className="md:absolute top-0 left-0 md:w-56 lg:left-6">
+        <motion.div
+          className="lg:absolute top-0 left-0 md:w-56 lg:left-6"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">Elite team</h1>
           <p className="text-primary">
             Personalized coaching and cutting-edge technology
           </p>
-        </div>
+        </motion.div>
 
-        <div className="md:absolute top-[30%] left-0 md:w-56 sm:top-0 sm:-translate-x-1/2 sm:left-1/2 ">
+        <motion.div
+          className="lg:absolute top-[30%] left-0 md:w-56 sm:top-0 lg:-translate-x-1/2 lg:left-1/2"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">Video Analysis</h1>
           <p className="text-primary">
             Analyze performance, refine tactics, and improve with expert
             feedback
           </p>
-        </div>
+        </motion.div>
 
-        <div className="md:absolute bottom-0 left-0 md:w-56">
+        <motion.div
+          className="lg:absolute bottom-0 left-0 md:w-56"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">Pro-Club Clinics & Games</h1>
           <p className="text-primary">
             Exclusive access to top-level La Liga clubs such as Atletico de
             Madrid, Racing de Santander or Real Oviedo
           </p>
-        </div>
+        </motion.div>
+
         {/* Right column */}
-        <div className="md:absolute top-0 right-0 sm:text-right md:w-56">
+        <motion.div
+          className="lg:absolute top-0 right-0 lg:text-right md:w-56"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">Football Reports</h1>
           <p className="text-primary">
             Track progress, receive feedback, and set targets for improvement
           </p>
-        </div>
+        </motion.div>
 
-        <div className="md:absolute bottom-[30%] left-0 md:w-56 sm:bottom-0 sm:-translate-x-1/2 sm:left-1/2 ">
+        <motion.div
+          className="lg:absolute bottom-[30%] left-0 md:w-56 sm:bottom-0 lg:-translate-x-1/2 lg:left-1/2"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">National Tournaments</h1>
           <p className="text-primary">
             Showcase talent on a national stage and catch the eye of scouts
           </p>
-        </div>
+        </motion.div>
 
-        <div className="md:absolute bottom-0 right-0 md:w-56 sm:text-right">
+        <motion.div
+          className="lg:absolute bottom-0 right-0 md:w-56 lg:text-right"
+          initial={{ y: 100 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <h1 className="text-2xl font-bold">High Level Competition</h1>
           <p className="text-primary">
             Compete against top-tier opponents, developing resilience and skills
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/sections/TeamManagement.jsx
+++ src/sections/TeamManagement.jsx
@@ -4,40 +4,58 @@ import {
   startRunning,
   soccerBall,
 } from "../assets/images/gallery";
+import { motion } from "framer-motion"; // Import motion from framer-motion
 
 const TeamManagement = () => {
   return (
-    <div className="min-h-screen bg-white relative mb-10">
-      <div className="ml-auto h-full w-full md:w-4/5 lg:w-[70%] -z-20">
-        <img
+    <div className="h-screen md:min-h-screen bg-white relative mb-10">
+      <div className="ml-auto h-full w-full md:w-4/5 lg:w-[70%] -z-20 ">
+        <div className="bg-white/50 h-full w-full absolute backdrop-blur-sm sm:hidden"></div>
+        <motion.img // Use motion.img for the parallax effect
           src={soccerBall}
           alt="soccer ball"
           className="w-full h-full object-cover"
+          initial={{ y: 50 }}
+          whileInView={{ y: 0 }}
+          transition={{ duration: 0.5 }}
         />
       </div>
 
       <div className="z-20 absolute top-0 w-full h-full flex items-center ">
         <div className="sm:w-[70%] sm:border-4 border-white relative">
-          <img
+          <motion.img // Use motion.img for the parallax effect
             src={manager1}
             alt=""
             className="mb-4 border-4 border-white sm:border-0 sm:mb-0"
+            initial={{ y: 50 }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.5 }}
           />
           <div className="sm:absolute right-0 top-1/2 sm:-translate-y-1/2 sm:translate-x-1/2 grid gap-4">
-            <div className="sm:w-96 h-52 border-4 border-white">
+            <motion.div // Wrap images in motion.div for parallax effect
+              className="sm:w-96 h-52 border-4 border-white"
+              initial={{ y: 50 }}
+              whileInView={{ y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
               <img
                 src={startRunning}
                 alt=""
                 className="h-full w-full object-cover"
               />
-            </div>
-            <div className="sm:w-96 h-52 border-4 border-white">
+            </motion.div>
+            <motion.div // Wrap images in motion.div for parallax effect
+              className="sm:w-96 h-52 border-4 border-white"
+              initial={{ y: 50 }}
+              whileInView={{ y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
               <img
                 src={stretchGround}
                 alt=""
                 className="h-full w-full object-cover"
               />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
